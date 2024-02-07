f4f41516f8fcc3dc2ee4759ef51d83addea248ae
DESKTOP-NR6TVAI\LAPTOP <amiraelsaftytraining@gmail.com>
2024-02-07 15:13:41 +0200

    initial project. added empty home screen.

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app_todo_c10_sun3/home/home_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: HomeScreen.routeName,
+      routes: {HomeScreen.routeName: (context) => HomeScreen()},
+    );
+  }
+}
